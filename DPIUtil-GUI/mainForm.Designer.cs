145b9d41e4dc96573ce2c259bf2f33033231114f
Erdem <75310852+Exinie@users.noreply.github.com>
2024-12-18 21:45:58 +0300

    First commit: Draft

---
+++ DPIUtil-GUI/mainForm.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace DPIUtil_GUI
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbxUseWhitelist = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnReinstallService = new System.Windows.Forms.Button();
+            this.btnRestartService = new System.Windows.Forms.Button();
+            this.btnStopService = new System.Windows.Forms.Button();
+            this.btnStartService = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtWhitelistFile = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.ItemSize = new System.Drawing.Size(64, 24);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(484, 251);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 28);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(476, 219);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Dashboard";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbxUseWhitelist);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.lbl2);
+            this.groupBox2.Location = new System.Drawing.Point(8, 112);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(462, 99);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Settings";
+            // 
+            // cbxUseWhitelist
+            // 
+            this.cbxUseWhitelist.AutoSize = true;
+            this.cbxUseWhitelist.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cbxUseWhitelist.Location = new System.Drawing.Point(6, 70);
+            this.cbxUseWhitelist.Name = "cbxUseWhitelist";
+            this.cbxUseWhitelist.Size = new System.Drawing.Size(121, 24);
+            this.cbxUseWhitelist.TabIndex = 1;
+            this.cbxUseWhitelist.Text = "Use Whitelist";
+            this.cbxUseWhitelist.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(446, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl2.Location = new System.Drawing.Point(2, 16);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(146, 20);
+            this.lbl2.TabIndex = 0;
+            this.lbl2.Text = "Service parameters";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnReinstallService);
+            this.groupBox1.Controls.Add(this.btnRestartService);
+            this.groupBox1.Controls.Add(this.btnStopService);
+            this.groupBox1.Controls.Add(this.btnStartService);
+            this.groupBox1.Controls.Add(this.lbl1);
+            this.groupBox1.Controls.Add(this.lblStatus);
+            this.groupBox1.Location = new System.Drawing.Point(8, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Service";
+            // 
+            // btnReinstallService
+            // 
+            this.btnReinstallService.Location = new System.Drawing.Point(279, 63);
+            this.btnReinstallService.Name = "btnReinstallService";
+            this.btnReinstallService.Size = new System.Drawing.Size(85, 30);
+            this.btnReinstallService.TabIndex = 4;
+            this.btnReinstallService.Text = "Reinstall";
+            this.btnReinstallService.UseVisualStyleBackColor = true;
+            // 
+            // btnRestartService
+            // 
+            this.btnRestartService.Location = new System.Drawing.Point(188, 63);
+            this.btnRestartService.Name = "btnRestartService";
+            this.btnRestartService.Size = new System.Drawing.Size(85, 30);
+            this.btnRestartService.TabIndex = 3;
+            this.btnRestartService.Text = "Restart";
+            this.btnRestartService.UseVisualStyleBackColor = true;
+            // 
+            // btnStopService
+            // 
+            this.btnStopService.Location = new System.Drawing.Point(97, 63);
+            this.btnStopService.Name = "btnStopService";
+            this.btnStopService.Size = new System.Drawing.Size(85, 30);
+            this.btnStopService.TabIndex = 2;
+            this.btnStopService.Text = "Stop";
+            this.btnStopService.UseVisualStyleBackColor = true;
+            // 
+            // btnStartService
+            // 
+            this.btnStartService.Location = new System.Drawing.Point(6, 63);
+            this.btnStartService.Name = "btnStartService";
+            this.btnStartService.Size = new System.Drawing.Size(85, 30);
+            this.btnStartService.TabIndex = 1;
+            this.btnStartService.Text = "Start";
+            this.btnStartService.UseVisualStyleBackColor = true;
+            // 
+            // lbl1
+            // 
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(6, 16);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(90, 30);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Status  - ";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblStatus.ForeColor = System.Drawing.Color.White;
+            this.lblStatus.Location = new System.Drawing.Point(102, 16);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(95, 30);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "-";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtWhitelistFile);
+            this.tabPage2.Location = new System.Drawing.Point(4, 28);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(476, 219);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Whitelist";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtWhitelistFile
+            // 
+            this.txtWhitelistFile.Location = new System.Drawing.Point(8, 6);
+            this.txtWhitelistFile.Multiline = true;
+            this.txtWhitelistFile.Name = "txtWhitelistFile";
+            this.txtWhitelistFile.Size = new System.Drawing.Size(460, 206);
+            this.txtWhitelistFile.TabIndex = 0;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 251);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DPIUtil";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cbxUseWhitelist;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnReinstallService;
+        private System.Windows.Forms.Button btnRestartService;
+        private System.Windows.Forms.Button btnStopService;
+        private System.Windows.Forms.Button btnStartService;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtWhitelistFile;
+    }
+}
+
